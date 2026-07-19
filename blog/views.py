6c088e1d434fcde27466ce629adb07a5998eefbe
Addison Kopp <addisonkopp@Addisons-MacBook-Air-203.local>
2026-07-19 12:23:08 -0400

    Completed Django Girls blog tutorial

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from urllib import request
 
-# Create your views here.
+from django.shortcuts import render
+from django.utils import timezone 
+from .models import Post
+
+# Create your views here.def post_list(request):
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
