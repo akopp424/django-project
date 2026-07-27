32a89273ebd4e1d9a6afcf71675ece8fbe240d77
Addison Kopp <addisonkopp@Addisons-MacBook-Air-203.local>
2026-07-26 20:41:55 -0400

    Added comment model and secuirty

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.conf import settings
+from django.utils import timezone
+
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -13,3 +12,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
